4165e38b04de49f5acae8831140d8932c6e6a1ed
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-02-08 15:17:48 +0530

    Created API files

---
+++ src/api.js
@@ -0,0 +1,37 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+
+//Getting the month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+//Getting the day
+const getCurrentDay = () => {
+  const day = new Date().getDate() + 1;
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentDay}-${currentMonth}`;
+const lastYear = `${currentYear - 1}-${currentDay}-${currentMonth}`;
+const nextYear = `${currentYear + 1}-${currentDay}-${currentMonth}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGamesURL = () => `${base_url}${popular_games}`;
+
+console.log(popularGamesURL());
