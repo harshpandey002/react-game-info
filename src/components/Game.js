e5f53adc2e1df1745aedc83321fe10a5e53c3309
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-02-13 14:15:53 +0530

    Routing

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 //Redux
 import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailActions";
+import { Link } from "react-router-dom";
 
 const Game = ({ name, released, image, id }) => {
   const dispatch = useDispatch();
@@ -12,9 +13,11 @@ const Game = ({ name, released, image, id }) => {
   };
   return (
     <StyledGame onClick={loadDetailHandler}>
-      <h3>{name}</h3>
-      <p>{released} Date</p>
-      <img src={image} alt={name} />
+      <Link to={`/game/${id}`}>
+        <h3>{name}</h3>
+        <p>{released} Date</p>
+        <img src={image} alt={name} />
+      </Link>
     </StyledGame>
   );
 };
@@ -24,6 +27,7 @@ const StyledGame = styled(motion.div)`
   box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
+  cursor: pointer;
   img {
     width: 100%;
     height: 40vh;
--- src/reducers/detailReducer.js
+++ src/reducers/detailReducer.js
@@ -1,5 +1,5 @@
 const initialState = {
-  game: {},
+  game: { platforms: [] },
   screen: [],
 };
 
