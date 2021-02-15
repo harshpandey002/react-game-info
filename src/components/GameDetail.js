86070e9c8dce67a6356d752b8c7950d61bb4f4ce
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-02-15 15:37:55 +0530

    Component Transition

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -8,7 +8,7 @@ import { useHistory } from "react-router-dom";
 
 //Media Resize
 import { smallImage } from "../util";
-const GameDetail = () => {
+const GameDetail = ({ pathId }) => {
   const history = useHistory();
   //Exit Detail
   const exitDetailHandler = (e) => {
@@ -25,10 +25,10 @@ const GameDetail = () => {
     <>
       {!isLoading && (
         <CardShadow className="shadow" onClick={exitDetailHandler}>
-          <Detail>
+          <Detail layoutId={pathId}>
             <Stats>
               <div className="rating">
-                <h3>{game.name}</h3>
+                <motion.h3 layoutId={`title $(pathId)`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
               </div>
               <Info>
@@ -41,7 +41,11 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={smallImage(game.background_image, 1280)} alt="game" />
+              <motion.img
+                layoutId={`image $(pathId)`}
+                src={game.background_image}
+                alt="game"
+              />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
