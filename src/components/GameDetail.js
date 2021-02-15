338303a39474d9394c850cd1cedc49ba2c6209a5
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-02-15 16:09:21 +0530

    Rating Stars Logic

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -17,6 +17,10 @@ import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
 
+//Star
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
+
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
   //Exit Detail
@@ -27,6 +31,21 @@ const GameDetail = ({ pathId }) => {
       history.push("/");
     }
   };
+
+  //Get Stars
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt="star" key={i} src={starFull} />);
+      } else {
+        stars.push(<img alt="star" key={i} src={starEmpty} />);
+      }
+    }
+    return stars;
+  };
+
   //GET PLATFORM IMAGES
   const getPlatform = (platform) => {
     switch (platform) {
@@ -55,6 +74,7 @@ const GameDetail = ({ pathId }) => {
               <div className="rating">
                 <motion.h3 layoutId={`title $(pathId)`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
+                {getStars()}
               </div>
               <Info>
                 <h3>Platforms</h3>
@@ -136,6 +156,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
